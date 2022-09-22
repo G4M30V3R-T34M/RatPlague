3db7a537363afb22019010b5ff7fdad7e6058ef8
Neengash <pepmanas93@gmail.com>
2022-09-22 20:09:40 +0200

    Fix asset menu option scriptable name

--- Assets/Scripts/ScriptableObjects/BaseBuildingScriptable.cs
+++ Assets/Scripts/ScriptableObjects/BaseBuildingScriptable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BaseIconScriptable", menuName = "Scriptables/BaseIconScriptable", order = 2)]
+[CreateAssetMenu(fileName = "BaseBuildingScriptable", menuName = "Scriptables/BaseBuildingScriptable", order = 2)]
 public class BaseBuildingScriptable : ScriptableObject
 {
     public Color ColorOnMouseEnter;
