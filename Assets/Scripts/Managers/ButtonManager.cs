9e0e784eb3c3e73b8446cde3e2391e6f68e22340
Jaume Riera <jaumerg95@gmail.com>
2022-09-22 19:45:04 +0200

    Add Resume game to button manager

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     public void MainMenu() {
+        Time.timeScale = 1;
         SceneManager.ChangeScene(Scenes.MainMenu);
     }
 
     public void GamePlay() {
+        Time.timeScale = 1;
         SceneManager.ChangeScene(Scenes.GamePlay);
     }
 
     public void Tutorial() {
+        Time.timeScale = 1;
         SceneManager.ChangeScene(Scenes.Tutorial);
     }
 
     public void Scores() {
+        Time.timeScale = 1;
         SceneManager.ChangeScene(Scenes.BestScores);
     }
 
     public void Credits() {
+        Time.timeScale = 1;
         SceneManager.ChangeScene(Scenes.Credits);
+    }
+
+    public void Resume() {
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
     }
 
     public void Exit() {
