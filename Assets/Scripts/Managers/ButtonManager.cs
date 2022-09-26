22b6612f0ad9f6d7cd2c346212570e11df68c7f2
Neengash <pepmanas93@gmail.com>
2022-09-26 12:44:31 +0200

    Scene Transitions

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -6,27 +6,27 @@ public class ButtonManager : MonoBehaviour
 {
     public void MainMenu() {
         Time.timeScale = 1;
-        SceneManager.ChangeScene(Scenes.MainMenu);
+        SceneManager.Instance.ChangeScene(Scenes.MainMenu);
     }
 
     public void GamePlay() {
         Time.timeScale = 1;
-        SceneManager.ChangeScene(Scenes.GamePlay);
+        SceneManager.Instance.ChangeScene(Scenes.GamePlay);
     }
 
     public void Tutorial() {
         Time.timeScale = 1;
-        SceneManager.ChangeScene(Scenes.Tutorial);
+        SceneManager.Instance.ChangeScene(Scenes.Tutorial);
     }
 
     public void Scores() {
         Time.timeScale = 1;
-        SceneManager.ChangeScene(Scenes.BestScores);
+        SceneManager.Instance.ChangeScene(Scenes.BestScores);
     }
 
     public void Credits() {
         Time.timeScale = 1;
-        SceneManager.ChangeScene(Scenes.Credits);
+        SceneManager.Instance.ChangeScene(Scenes.Credits);
     }
 
     public void Resume() {
--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Feto;
 
-public class SceneManager : MonoBehaviour
+public class SceneManager : Singleton<SceneManager>
 {
-    public static void ChangeScene(Scenes scene) {
-        UnityEngine.SceneManagement.SceneManager.LoadScene((int)scene);
+    Animator animator;
+
+    int nextScene;
+    bool animating = false;
+
+    private void Start() {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void ChangeScene(Scenes scene) {
+        if (!animating) {
+            animating = true;
+            nextScene = (int)scene;
+            animator.SetTrigger("SceneOut");
+        }
+    }
+
+    public void DoChangeScene() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
     }
 }
