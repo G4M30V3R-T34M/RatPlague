9c1b68a50c0f4da979bfb130dc1e9efc3950bd52
Jaume Riera <jaumerg95@gmail.com>
2022-10-07 14:17:30 +0200

    Add translations to game over

--- Assets/Scripts/GameOverMenu/LoadGameOverReason.cs
+++ Assets/Scripts/GameOverMenu/LoadGameOverReason.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Localization;
 
 public class LoadGameOverReason : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI gameOverReasonText;
 
-    [SerializeField] string NoRatsReason, TooManyRatsReason, QuarantineReason;
+    [SerializeField] LocalizedString NoRatsReason, TooManyRatsReason, QuarantineReason;
 
     private void Start() {
         switch (EndGameManager.Instance.endGameCause) {
             case GameOverCondition.NoRats:
-                gameOverReasonText.SetText(NoRatsReason);
+                gameOverReasonText.SetText(NoRatsReason.GetLocalizedString());
                 break;
             case GameOverCondition.TooManyRats:
-                gameOverReasonText.SetText(TooManyRatsReason);
+                gameOverReasonText.SetText(TooManyRatsReason.GetLocalizedString());
                 break;
             case GameOverCondition.Quarantine:
-                gameOverReasonText.SetText(QuarantineReason);
+                gameOverReasonText.SetText(QuarantineReason.GetLocalizedString());
                 break;
         }
     }
